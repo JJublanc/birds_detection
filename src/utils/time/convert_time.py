d8bcafa7559f782ba99212be70a538704a6e708c
jjublanc <johan.jublanc@gmail.com>
2023-01-02 12:00:04 +0100

    feat(exp): run_id=9d6a48c33ad54f90beb1c7c7bfde6f63

--- src/utils/time/convert_time.py
+++ src/utils/time/convert_time.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
-
 def get_date_from_string(s_date):
     date_patterns = [
-        "%Y-%m-%d %H:%M:%S",
         "%Y:%m:%d %H:%M:%S",
+        "%Y-%m-%d %H:%M:%S",
         "%Y-%m-%d",
         "%d-%m-%Y",
     ]
 
-    try:
-        for pattern in date_patterns:
+    for pattern in date_patterns:
+        try:
             return datetime.strptime(s_date, pattern)
-    except ValueError:
-        print(f"Date {s_date} not in expected format: %s")
+        except ValueError:
+            print("Date is not in expected format: %s" % (s_date))
+
+
