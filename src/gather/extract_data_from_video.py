a35e6220f60305a9099b246e1a99d8af2dba06f5
jjublanc <johan.jublanc@gmail.com>
2023-01-07 09:08:52 +0100

    WIP: video

--- src/gather/extract_data_from_video.py
+++ src/gather/extract_data_from_video.py
@@ -1,19 +1,46 @@
-import cv2
+import datetime
 import os
 
+import cv2
+import piexif
+from PIL import Image
+
+from config import INPUT_IMAGES_FOLDER, MAIN_FOLDER
+
 DATA_PATH = "data_video"
-VIDEO_NAME = 'video/birds_video.mp4'
+VIDEO_PATH = os.path.join(MAIN_FOLDER, "video/birds_video.mp4")
 
-def extract():
-  vidcap = cv2.VideoCapture(os.path.join(DATA_PATH, VIDEO_NAME))
-  success, image = vidcap.read()
-  count = 0
-  while success:
-    cv2.imwrite(os.path.join(DATA_PATH, "1_input_images/frame%d.jpg" % count), image)
-    success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 50
-    vidcap.set(cv2.CAP_PROP_POS_FRAMES, count)
 
-if __name__=="__main__":
-  extract()
+def extract(period=50):
+    video = cv2.VideoCapture(VIDEO_PATH)
+    success = True
+    count = 0
+    now = datetime.datetime.now()
+
+    while success:
+        date_time = now + datetime.timedelta(seconds=count)
+        date_time_str = date_time.strftime("%Y:%m:%d %H:%M:%S")
+
+        success, frame = video.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(frame)
+
+        exif_dict = {"0th": {piexif.ImageIFD.DateTime: None}}
+        exif_dict["0th"][piexif.ImageIFD.DateTime] = date_time_str
+        exif_bytes = piexif.dump(exif_dict)
+
+        # Save the image as a JPEG file with the metadata
+        image.save(
+            os.path.join(INPUT_IMAGES_FOLDER, f"frame_{count}.jpg"),
+            "JPEG",
+            exif=exif_bytes,
+            optimize=True,
+        )
+
+        print("Read a new frame: ", success)
+        count += period
+        video.set(cv2.CAP_PROP_POS_FRAMES, count)
+
+
+if __name__ == "__main__":
+    extract()
